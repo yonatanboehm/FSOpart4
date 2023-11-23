55ed16f1bae8c765081e9d92eed3d11b4141fd5f
Yonatan Boehm <yonatanboehm@Yonatans-MacBook-Pro.local>
2023-11-23 21:38:33 +0200

    exercise 4.18 and 4.19

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,16 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -11,10 +21,11 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
 
-    const users = await User.find({})
-    const user = users[Math.floor(Math.random() * users.length)]
-
-    console.log(user)
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
 
     const blog = new Blog({
       url: body?.url,
